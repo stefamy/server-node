3e1316c6c04e63c1b25e075e17bb7abfdf437fc5
Amy Stefani <amystefani1@gmail.com>
2020-04-17 10:22:23 -0700

    fix typo in question.model. server functional with  mongoose

--- models/questions/questions.model.server.js
+++ models/questions/questions.model.server.js
@@ -1,7 +1,4 @@
-const mongoose = require('mongoose')
-const questionsSchema = require('./questions.schema.server')
-const questionsModel = mongoose.model(
-    'QuestionModel',
-    questionsSchema
-)
-module.exports = questionsModel
+const mongoose = require("mongoose");
+const questionsSchema = require("./questions.schema.server");
+const questionsModel = mongoose.model("QuestionsModel", questionsSchema);
+module.exports = questionsModel;
