50bf393c1ce1203c0d756f39c7f7876c02201aeb
Amy Stefani <amystefani1@gmail.com>
2020-04-17 10:24:34 -0700

    fixing another typo

--- services/question.service.server.js
+++ services/question.service.server.js
@@ -2,6 +2,6 @@ const questionsDao = require("../daos/questions.dao.server");
 
 const findAllQuestions = () => questionsDao.findAllQuestions();
 const findQuestionById = (qid) => questionsDao.findQuestionById(qid);
-const findQuestionsForQuiz = (qid) => questionsDaoDao.findQuestionsForQuiz(qid);
+const findQuestionsForQuiz = (qid) => questionsDao.findQuestionsForQuiz(qid);
 
 module.exports = { findAllQuestions, findQuestionById, findQuestionsForQuiz };
